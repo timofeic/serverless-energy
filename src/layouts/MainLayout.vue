4773407b02cef582a89893de3588a9afa2f01b19
Tim Cheung <cheungt@amazon.com>
2020-03-09 14:08:34 +0000

    Updating main layout and profile page

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,44 +1,90 @@
 <template>
   <q-layout view="lHh Lpr lFf">
-    <q-header elevated>
+    <q-header>
       <q-toolbar>
         <q-btn
           flat
           dense
           round
+          @click="leftDrawerOpen = !leftDrawerOpen"
           icon="menu"
           aria-label="Menu"
-          @click="leftDrawerOpen = !leftDrawerOpen"
         />
 
-        <q-toolbar-title>
-          Quasar App
-        </q-toolbar-title>
-
-        <div>Quasar v{{ $q.version }}</div>
+        <q-btn
+          to="/auth"
+          flat
+          icon-right="account_circle"
+          label="Login"
+          class="absolute-right" />
       </q-toolbar>
+      <q-img
+        src="statics/hotairballoon.jpg"
+        class="header-image absolute-top" />
     </q-header>
 
     <q-drawer
-      v-model="leftDrawerOpen"
-      show-if-above
-      bordered
-      content-class="bg-grey-1"
-    >
-      <q-list>
-        <q-item-label
-          header
-          class="text-grey-8"
-        >
-          Essential Links
-        </q-item-label>
-        <EssentialLink
-          v-for="link in essentialLinks"
-          :key="link.title"
-          v-bind="link"
-        />
-      </q-list>
-    </q-drawer>
+        v-model="leftDrawerOpen"
+        show-if-above
+        :width="250"
+        :breakpoint="400"
+      >
+        <q-scroll-area
+          style="height: calc(100% - 150px); margin-top: 150px; border-right: 1px solid #ddd">
+          <q-list padding>
+            <q-item
+            to="/"
+            exact
+            clickable
+            v-ripple>
+              <q-item-section avatar>
+                <q-icon name="photo" />
+              </q-item-section>
+
+              <q-item-section>
+                Photos
+              </q-item-section>
+            </q-item>
+            <q-item
+              to="/albums"
+              exact
+              clickable
+              v-ripple>
+              <q-item-section avatar>
+                <q-icon name="album" />
+              </q-item-section>
+
+              <q-item-section>
+                Albums
+              </q-item-section>
+            </q-item>
+            <q-item
+              to="/settings"
+              exact
+              clickable
+              v-ripple>
+              <q-item-section avatar>
+                <q-icon name="settings" />
+              </q-item-section>
+
+              <q-item-section>
+                Settings
+              </q-item-section>
+            </q-item>
+          </q-list>
+        </q-scroll-area>
+
+        <q-img class="absolute-top" src="statics/hotairballoon.jpg" style="height: 150px">
+          <div
+            class="absolute-bottom bg-transparent"
+            v-if="isLoggedIn">
+            <q-avatar size="56px" class="q-mb-sm">
+              <img src="https://cdn.quasar.dev/img/boy-avatar.png">
+            </q-avatar>
+            <div class="text-weight-bold">{{ user.username }}</div>
+          </div>
+        </q-img>
+      </q-drawer>
 
     <q-page-container>
       <router-view />
@@ -47,57 +93,54 @@
 </template>
 
 <script>
-import EssentialLink from 'components/EssentialLink'
+import { openURL } from 'quasar'
 
 export default {
-  name: 'MainLayout',
+  name: 'MyLayout',
 
-  components: {
-    EssentialLink
-  },
-
-  data () {
+  data() {
     return {
       leftDrawerOpen: false,
-      essentialLinks: [
-        {
-          title: 'Docs',
-          caption: 'quasar.dev',
-          icon: 'school',
-          link: 'https://quasar.dev'
-        },
-        {
-          title: 'Github',
-          caption: 'github.com/quasarframework',
-          icon: 'code',
-          link: 'https://github.com/quasarframework'
-        },
-        {
-          title: 'Discord Chat Channel',
-          caption: 'chat.quasar.dev',
-          icon: 'chat',
-          link: 'https://chat.quasar.dev'
-        },
-        {
-          title: 'Forum',
-          caption: 'forum.quasar.dev',
-          icon: 'record_voice_over',
-          link: 'https://forum.quasar.dev'
-        },
-        {
-          title: 'Twitter',
-          caption: '@quasarframework',
-          icon: 'rss_feed',
-          link: 'https://twitter.quasar.dev'
-        },
-        {
-          title: 'Facebook',
-          caption: '@QuasarFramework',
-          icon: 'public',
-          link: 'https://facebook.quasar.dev'
-        }
-      ]
+      signedIn: false,
+    };
+  },
+  computed: {
+    isLoggedIn () {
+      return this.signedIn
+    }
+  },
+  mounted () {
+    this.$AmplifyEventBus.$on('authState', info => {
+      this.signedIn = true
+    })
+  },
+  beforeCreate () {
+    this.$Auth.currentAuthenticatedUser()
+      .then(user => {
+        this.user = user
+        this.signedIn = true
+      })
+      .catch(() => console.log('not signed in...'))
+  },
+  methods: {
+    openURL,
+    async signOut () {
+      await this.$Auth.signOut()
+        .then(data => console.log(data))
+        .catch(err => console.log(err))
+      this.signedIn = false
+      parent.signedIn = false
+      this.$router.push({ name: 'auth' })
     }
   }
-}
+};
 </script>
+
+<style lang="scss">
+  .header-image {
+    height: 100%;
+    z-index: -1;
+    opacity: 0.5;
+    filter: grayscale(100%)
+  }
+</style>
