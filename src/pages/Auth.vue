c0f8ee0ffdfdeccf78afe7bb9f79e78003c57f6f
Tim Cheung <cheungt@amazon.com>
2020-03-09 12:27:42 +0000

    Initial commit

---
+++ src/pages/Auth.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="auth">
+    <amplify-authenticator v-bind:authConfig="authConfig"></amplify-authenticator>
+  </div>
+</template>
+<script>
+export default {
+  name: 'MyComponent',
+  props: [],
+  data () {
+    return {
+      authConfig: {
+          usernameAttributes: 'email',
+          signUpConfig: {
+            header: 'My Customized Sign Up',
+            hideAllDefaults: true,
+            defaultCountryCode: '1',
+            signUpFields: [
+              {
+                label: 'email',
+                key: 'username',
+                required: true,
+                displayOrder: 1,
+                type: 'string',
+              },
+              {
+                label: 'Password',
+                key: 'password',
+                required: true,
+                displayOrder: 2,
+                type: 'password'
+              },
+              {
+                label: 'Phone Number',
+                key: 'phone_number',
+                required: true,
+                displayOrder: 3,
+                type: 'string'
+              },
+              {
+                label: 'First Name',
+                key: 'first_name',
+                required: false,
+                displayOrder: 4,
+                type: 'string',
+                custom: true
+              },
+                            {
+                label: 'Last Name',
+                key: 'last_name',
+                required: false,
+                displayOrder: 5,
+                type: 'string',
+                custom: true
+              }
+            ]
+          }
+        }
+      }
+    }
+  }
+</script>
+<style scoped>
+</style>
---
+++ src/router/routes.js
@@ -0,0 +1,44 @@
+
+const routes = [
+  {
+    path: '/',
+    component: () => import('layouts/MainLayout.vue'),
+    children: [
+      { path: '', component: () => import('pages/Index.vue') }
+    ]
+  },
+  {
+    path: '/auth',
+    component: () => import('layouts/MyLayout.vue'),
+    children: [{
+      path: '',
+      name: 'auth',
+      component: () => import('pages/Auth.vue'),
+      meta: {
+        requiresAuth: false
+      }
+    }]
+  },
+  {
+    path: '/profile',
+    component: () => import('layouts/MyLayout.vue'),
+    children: [{
+      name: 'profile',
+      path: '',
+      component: () => import('pages/Profile.vue'),
+      meta: {
+        requiresAuth: true
+      }
+    }]
+  }
+]
+
+// Always leave this as last one
+if (process.env.MODE !== 'ssr') {
+  routes.push({
+    path: '*',
+    component: () => import('pages/Error404.vue')
+  })
+}
+
+export default routes
